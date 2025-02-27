95f6c33e965d2750d0e2845dd28681bad9931adc
Jonathansampedromontes <jonathansampedromontes@alumno.ieselrincon.es>
2025-02-27 11:57:09 +0000

    cuadros de eventos hecho

--- material-kit-react-main/src/pages/Presentation/sections/data/designBlocksData.js
+++ material-kit-react-main/src/pages/Presentation/sections/data/designBlocksData.js
@@ -35,7 +35,7 @@ export default [
       },
       {
         image: "/Aguaje.png",
-        name: "AGUAJE GRUPO",
+        name: "AGUAJE GRUP",
       },
       {
         image: "/virgenSocorro.png",
